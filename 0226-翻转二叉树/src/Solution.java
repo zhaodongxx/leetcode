2671022a11d252ef485807d2991fc04cdfb10b9e
zhaodongxx <zhaodongxx@outlook.com>
2021-05-08 17:36:24 +0800

    增加2道题

---
+++ 0226-翻转二叉树/src/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * @author zhaodong5
+ * @date 2021/5/8 16:12
+ */
+public class Solution {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
